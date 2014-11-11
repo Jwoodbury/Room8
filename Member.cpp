c0a4556956246cbc346084ffadb369538813cb33
chasefarmer2808 <chasefarmer2808@aol.com>
2014-11-10 19:53:20 -0800

    Create Member.cpp

---
+++ Member.cpp
@@ -0,0 +1,22 @@
+#include "Room8.h"
+
+using namespace std;
+
+class Member {
+	name = " ";
+	Member(int adminStatus) {
+		
+
+	}
+
+	//void memberSettings();
+	string getDescription();
+	void setDescription(string desc);
+	void setName(string _name);
+	string getName();
+	void setStatus(int _n);
+	int getStatus();
+
+	string name;
+};
+
